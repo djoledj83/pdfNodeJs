63143fdabe578f70e058e0bcec708e49d5928943
djoledj83 <djoledj@hotmail.com>
2024-10-10 15:32:10 +0200

    Dopuna navbara

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const path = require('path');
+const homeRoute = require('./routes/home-route')
 
 const app = express();
 const PORT = 2200;
@@ -9,5 +10,6 @@ app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(homeRoute.routes)
 
 app.listen(PORT, () => console.log(`App is running on url: http://localhost:${PORT}`));
