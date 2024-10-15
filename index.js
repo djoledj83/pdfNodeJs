8a018146190075d25e8a75feaef38640ba56d43e
djoledj83 <djoledj@hotmail.com>
2024-10-15 14:58:10 +0200

    Sve update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const path = require('path');
-const homeRoute = require('./routes/home-route')
+const homeRoutes = require('./routes/home-route')
 
 const app = express();
 const PORT = 2200;
@@ -10,6 +10,7 @@ app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(homeRoute.routes)
+app.use('/docs', express.static(path.join(__dirname, 'docs')));
+app.use(homeRoutes.routes);
 
 app.listen(PORT, () => console.log(`App is running on url: http://localhost:${PORT}`));
--- routes/home-route.js
+++ routes/home-route.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { homeview } = require('../controllers/homeController');
+const { homeview, generatePdf } = require('../controllers/homeController');
 
 const router = express.Router();
 
 router.get('/', homeview);
+router.get('/download', generatePdf);
 
 module.exports = {
     routes: router
